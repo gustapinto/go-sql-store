d52a1c7350e135f8b94de7549b09abcde9dcc14e
Gustavo Henrique Pinto <ghp2201@gmail.com>
2024-12-25 21:51:50 -0300

    feat: add create database executor

--- pkg/executor/database.go
+++ pkg/executor/database.go
@@ -4,11 +4,12 @@ import (
 	"context"
 
 	gokvstore "github.com/gustapinto/go-kv-store"
+	"github.com/gustapinto/go-sql-store/pkg/operators/ddl"
 )
 
 var (
 	CreateDatabaseID                             = "CREATE_DATABASE"
-	CreateDatabaseParamsName              ctxKey = "CREATE_DATABASE_PARAMS_DATABASE"
+	CreateDatabaseParamsDatabaseName      ctxKey = "CREATE_DATABASE_PARAMS_DATABASE_NAME"
 	CreateDatabaseParamsCreateOrReplace   ctxKey = "CREATE_DATABASE_PARAMS_CREATE_OR_REPLACE"
 	CreateDatabaseParamsCreateIfNotExists ctxKey = "CREATE_DATABASE_PARAMS_CREATE_IF_NOT_EXISTS"
 	CreateDatabaseResponse                ctxKey = "CREATE_DATABASE_RESPONSE"
@@ -18,8 +19,28 @@ func CreateDatabaseAction() Action {
 	return Action{
 		ID: CreateDatabaseID,
 		Execute: func(rootCollection *gokvstore.Collection, in context.Context) (context.Context, ExecuteResult, error) {
-			// TODO
-			return in, nil, nil
+			name, ok := in.Value(CreateDatabaseParamsDatabaseName).(string)
+			if !ok {
+				return in, nil, valueMissingOrWithWrongTypeError(CreateDatabaseParamsDatabaseName)
+			}
+
+			createOrReplace, ok := in.Value(CreateDatabaseParamsCreateOrReplace).(bool)
+			if !ok {
+				return in, nil, valueMissingOrWithWrongTypeError(CreateDatabaseParamsCreateOrReplace)
+			}
+
+			createIfNotExists, ok := in.Value(CreateDatabaseParamsCreateIfNotExists).(bool)
+			if !ok {
+				return in, nil, valueMissingOrWithWrongTypeError(CreateDatabaseParamsCreateIfNotExists)
+			}
+
+			database := ddl.Database{Name: name}
+
+			if err := ddl.CreateDatabase(nil, database, createOrReplace, createIfNotExists); err != nil {
+				return in, nil, err
+			}
+
+			return in, successExecutionResult(), nil
 		},
 	}
 }
